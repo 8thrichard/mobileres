50ee9324cd0d271ce17021138a93f21192634fc9
Richard Boamah <richboam8@gmail.com>
2023-12-13 12:59:16 -0600

    Init

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuToggle = document.getElementById('mobile-menu');
+    const menu = document.querySelector('.menu');
+
+    mobileMenuToggle.addEventListener('click', function () {
+        menu.classList.toggle('show');
+        document.body.classList.toggle('menu-open');
+    });
+});
